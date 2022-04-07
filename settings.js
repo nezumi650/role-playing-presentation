39b01704bc88066bcfc0aeb16f864e9ca0b57dd2
nezumi650 <n_hirafuji@tokyootakumode.com>
2022-04-07 20:54:26 +0900

    微修正

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
-/***** １度に移動する距離(px) *****/
-const HOHABA_PX = 10;
+// １度に移動する距離(px)
+const HOHABA_PX = 20;
 
-/***** アバター画像の横幅(px) *****/
+// アバター画像の横幅(px)
 const AVATAR_WIDTH = 50;
 
-/***** コンテンツの大きさ(px) *****/
+// コンテンツの大きさ(px)
 const CONTAINER_HEIGHT = 500;
 const CONTAINER_WIDTH = 700;
 
@@ -13,11 +13,11 @@ const MAX_LEFT_CONTAINER = CONTAINER_WIDTH - AVATAR_WIDTH - 10;
 const MIN_TOP_CONTAINER = 30;
 const MAX_TOP_CONTAINER = CONTAINER_HEIGHT - AVATAR_WIDTH;
 
-/***** 廊下の縦幅(px) *****/
+// 廊下の縦幅(px)
 const MIN_TOP_ROUKA = 160;
 const MAX_TOP_ROUKA = 360;
 
-/***** ページ送り *****/
+// ページ送り
 const PrevPageKey = 'p'; //前のページを表示
 const NextPageKey = 'n'; //次のページを表示
 
